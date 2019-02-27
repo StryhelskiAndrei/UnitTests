4be7cf20934ca4a837fae79f296bdb534bbfec26
StryhelskiAndrei <d9d9pe4kin@mail.ru>
2019-02-27 16:07:48 +0300

    Initial commit

---
+++ MyCalc/MyCalc.Tests/MyCalcTests.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace MyCalc.Tests
+{
+    [TestClass]
+    public class MyCalcTests
+    {
+        [TestMethod]
+        public void Sum10and20_returned30()
+        {
+            int x = 10;
+            int y = 20;
+            int expected = 30;
+
+
+            MyCalc calc = new MyCalc();
+            int actual = calc.Sum(x, y);
+
+
+            Assert.AreEqual(expected, actual);
+        }
+    }
+}
---
+++ MyCalc/MyCalc.Tests/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("MyCalc.Tests")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("MyCalc.Tests")]
+[assembly: AssemblyCopyright("Copyright ©  2019")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("0f52b11e-0f91-44c4-a5db-a38b4491b50b")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
